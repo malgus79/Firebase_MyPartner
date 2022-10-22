007b8a1f0e07059461116be133f07746a4923d2b
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-22 11:42:50 -0300

    Keep the session active added

--- app/src/main/java/com/mypartner/MainActivity.kt
+++ app/src/main/java/com/mypartner/MainActivity.kt
@@ -1,37 +1,64 @@
 package com.mypartner
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //habilitar todos los proveedores de auth
-        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+        configAuth()
 
-        //esperar el resultado
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            //aca se procesa la respuesta
-            val response = IdpResponse.fromResultIntent(it.data)
+    }
 
-            if (it.resultCode == RESULT_OK) {
-                //usuario autenticado
-                val user = FirebaseAuth.getInstance().currentUser
-                if (user != null) {
-                    Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
-                }
+    private fun configAuth() {
+        firebaseAuth = FirebaseAuth.getInstance()
+        authStateListener = FirebaseAuth.AuthStateListener { auth ->
+            //usuario autenticado
+            if (auth.currentUser != null) {
+                supportActionBar?.title = auth.currentUser?.displayName
+            } else {
+
+                //habilitar todos los proveedores de auth
+                val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+
+                //esperar el resultado
+                registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                    //aca se procesa la respuesta
+                    val response = IdpResponse.fromResultIntent(it.data)
+
+                    if (it.resultCode == RESULT_OK) {
+                        //usuario autenticado
+                        val user = FirebaseAuth.getInstance().currentUser
+                        if (user != null) {
+                            Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }.launch(AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build())
             }
+        }
+    }
 
-        }.launch(AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build())
+    override fun onResume() {
+        super.onResume()
+        firebaseAuth.addAuthStateListener(authStateListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        firebaseAuth.removeAuthStateListener { authStateListener }
     }
 }
