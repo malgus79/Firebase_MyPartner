3369dd66c2d898757f6c9342183926b909173709
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-22 12:59:46 -0300

    Views and back control added

--- app/src/main/java/com/mypartner/MainActivity.kt
+++ app/src/main/java/com/mypartner/MainActivity.kt
@@ -3,15 +3,18 @@ package com.mypartner
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+import com.mypartner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var authStateListener: FirebaseAuth.AuthStateListener
 
@@ -27,12 +30,19 @@ class MainActivity : AppCompatActivity() {
                 if (user != null) {
                     Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
                 }
+            } else {
+                //se pulso atras
+                if (response == null) {
+                    Toast.makeText(this, "Hasta pronto", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
             }
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         configAuth()
 
@@ -44,8 +54,8 @@ class MainActivity : AppCompatActivity() {
             //usuario autenticado
             if (auth.currentUser != null) {
                 supportActionBar?.title = auth.currentUser?.displayName
+                binding.tvInit.visibility = View.VISIBLE
             } else {
-
                 //habilitar todos los proveedores de auth
                 val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
 
@@ -79,6 +89,13 @@ class MainActivity : AppCompatActivity() {
                 AuthUI.getInstance().signOut(this)
                     .addOnSuccessListener {
                         Toast.makeText(this, "Sesión finalizada", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            binding.tvInit.visibility = View.GONE
+                        } else {
+                            Toast.makeText(this, "No se pudo cerrar la sesión", Toast.LENGTH_SHORT).show()
+                        }
                     }
             }
         }
