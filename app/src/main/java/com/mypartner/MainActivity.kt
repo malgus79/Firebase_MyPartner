7321e6f9284cc47f7c50da3770600c7e5219e4a2
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-22 11:25:35 -0300

    Auth with email added

--- app/src/main/java/com/mypartner/MainActivity.kt
+++ app/src/main/java/com/mypartner/MainActivity.kt
@@ -2,10 +2,36 @@ package com.mypartner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //habilitar todos los proveedores de auth
+        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+
+        //esperar el resultado
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            //aca se procesa la respuesta
+            val response = IdpResponse.fromResultIntent(it.data)
+
+            if (it.resultCode == RESULT_OK) {
+                //usuario autenticado
+                val user = FirebaseAuth.getInstance().currentUser
+                if (user != null) {
+                    Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }.launch(AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build())
     }
 }
