18a241d3922fc0873fd3e4da1522f0e2e5d21cad
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-25 19:35:18 -0300

    Query token by user added

--- app/src/main/java/com/mypartner/order/OrderActivity.kt
+++ app/src/main/java/com/mypartner/order/OrderActivity.kt
@@ -12,6 +12,7 @@ import com.mypartner.R
 import com.mypartner.chat.ChatFragment
 import com.mypartner.databinding.ActivityOrderBinding
 import com.mypartner.entities.Order
+import com.mypartner.fcm.NotificationRS
 
 class OrderActivity : AppCompatActivity(), OnOrderListener, OrderAux {
 
@@ -23,13 +24,14 @@ class OrderActivity : AppCompatActivity(), OnOrderListener, OrderAux {
 
 //    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
-//    private val aValues: Array<String> by lazy {
-//        resources.getStringArray(R.array.status_value)
-//    }
-//
-//    private val aKeys: Array<Int> by lazy {
-//        resources.getIntArray(R.array.status_key).toTypedArray()
-//    }
+    //para poder extraer el arrays de valores y claves
+    private val aValues: Array<String> by lazy {
+        resources.getStringArray(R.array.status_value)
+    }
+
+    private val aKeys: Array<Int> by lazy {
+        resources.getIntArray(R.array.status_key).toTypedArray()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -72,43 +74,47 @@ class OrderActivity : AppCompatActivity(), OnOrderListener, OrderAux {
 //    private fun configAnalytics(){
 //        firebaseAnalytics = Firebase.analytics
 //    }
-//
-//    private fun notifyClient(order: Order){
-//        val db = FirebaseFirestore.getInstance()
-//
-//        db.collection(Constants.COLL_USERS)
-//            .document(order.clientId)
-//            .collection(Constants.COLL_TOKENS)
-//            .get()
-//            .addOnSuccessListener {
-//                var tokensStr = ""
-//                for (document in it){
-//                    val tokenMap = document.data
-//                    tokensStr += "${tokenMap.getValue(Constants.PROP_TOKEN)},"
-//                }
-//                if (tokensStr.length > 0) {
-//                    tokensStr = tokensStr.dropLast(1)
-//
-//                    var names = ""
-//                    order.products.forEach {
-//                        names += "${it.value.name}, "
-//                    }
-//                    names = names.dropLast(2)
-//
-//                    val index = aKeys.indexOf(order.status)
-//
-//                    val notificationRS = NotificationRS()
-//                    notificationRS.sendNotification(
-//                        "Tu pedido ha sido ${aValues[index]}",
-//                        names, tokensStr
-//                    )
-//                }
-//            }
-//            .addOnFailureListener {
-//                Toast.makeText(this, "Error al consultar los datos.", Toast.LENGTH_SHORT)
-//                    .show()
-//            }
-//    }
+
+    private fun notifyClient(order: Order){
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection(Constants.COLL_USERS)
+            .document(order.clientId)
+            .collection(Constants.COLL_TOKENS)
+            .get()
+            .addOnSuccessListener {
+                var tokensStr = ""
+                //crear una cadena de textos que puede concatenar los tokens separados por una coma
+                for (document in it){
+                    //tokenMap -> se llama igual al map declarado en la app MyClient
+                    val tokenMap = document.data
+                    tokensStr += "${tokenMap.getValue(Constants.PROP_TOKEN)},"
+                }
+                //si hubo un documento dentro de la consulta
+                if (tokensStr.length > 0) {
+                    tokensStr = tokensStr.dropLast(1)  //quitar la ultima coma, independientemente de cuantos doc se registraron
+
+                    var names = ""
+                    order.products.forEach {
+                        names += "${it.value.name}, "
+                    }
+                    names = names.dropLast(2)  //quitar los ultimos 2 caracteres (coma y espacio)
+
+                    //extraer el valor del estado actual
+                    val index = aKeys.indexOf(order.status)
+
+                    val notificationRS = NotificationRS()
+                    notificationRS.sendNotification(
+                        "Tu pedido ha sido ${aValues[index]}",
+                        names, tokensStr
+                    )
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Error al consultar los datos.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+    }
 
     //iniciar el fragment del chat
     override fun onStartChat(order: Order) {
@@ -132,18 +138,20 @@ class OrderActivity : AppCompatActivity(), OnOrderListener, OrderAux {
             .update(Constants.PROP_STATUS, order.status)
             .addOnSuccessListener {
                 Toast.makeText(this, "Orden actualizada.", Toast.LENGTH_SHORT).show()
-//                notifyClient(order)
-//                //Analytics
-//                firebaseAnalytics.logEvent(FirebaseAnalytics.Event.ADD_SHIPPING_INFO){
-//                    val products = mutableListOf<Bundle>()
-//                    order.products.forEach {
-//                        val bundle = Bundle()
-//                        bundle.putString("id_product", it.key)
-//                        products.add(bundle)
-//                    }
-//                    param(FirebaseAnalytics.Param.SHIPPING, products.toTypedArray())
-//                    param(FirebaseAnalytics.Param.PRICE, order.totalPrice)
-//                }
+
+                //aca en el listener exitoso con respecto a cambiar el estado de una orden
+                notifyClient(order)
+                //Analytics
+                firebaseAnalytics.logEvent(FirebaseAnalytics.Event.ADD_SHIPPING_INFO){
+                    val products = mutableListOf<Bundle>()
+                    order.products.forEach {
+                        val bundle = Bundle()
+                        bundle.putString("id_product", it.key)
+                        products.add(bundle)
+                    }
+                    param(FirebaseAnalytics.Param.SHIPPING, products.toTypedArray())
+                    param(FirebaseAnalytics.Param.PRICE, order.totalPrice)
+                }
             }
             .addOnFailureListener {
                 Toast.makeText(this, "Error al actualizar orden.", Toast.LENGTH_SHORT).show()
